d50560324cd213f7835cfb99832ea70b476113a5
Jakub Ciszewski <spaake123@gmail.com>
2016-03-01 15:14:25 +0100

    Podstawowe wyświetlanie i wprowadzanie umów

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,17 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/imie/{imie}", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request, $imie)
+    public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => $imie,
-        ));
+        return $this->redirectToRoute('view_umowy');
     }
 }
