76b0a74ff107342594f664621c89b2a306e542f3
Jakub Ciszewski <spaake123@gmail.com>
2016-03-03 09:34:25 +0100

    Przekazywanie umow na widok w panelu uzytkownika

--- src/UserBundle/Controller/ProfileController.php
+++ src/UserBundle/Controller/ProfileController.php
@@ -14,9 +14,20 @@ class ProfileController extends BaseController
     public function showAction()
     {
         $user = $this->getUser();
+
+        $em = $this->getDoctrine()->getManager();
+        $samorzad = $user->getSamorzad();
+        $repo = $em->getRepository('AppBundle:Umowa');
+        $qb = $repo->createQueryBuilder('u');
+        $query = $qb->where('u.samorzad='.$samorzad->getKod())->setMaxgetQuery();
+        $umowy = $query->getResult();
+
+
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
+
+
 
         return $this->render('FOSUserBundle:Profile:show.html.twig', array(
             'user' => $user,
