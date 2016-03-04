7512fd1608c33ba50581ea3143a0fc518a8e84c6
Jakub Ciszewski <spaake123@gmail.com>
2016-03-04 13:06:06 +0100

    Admin

---
+++ src/AppBundle/Controller/AdminController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin/umowy/dodaj", name="umowa_admin")
+     */
+    function NowaUmowaAdminAction(Request $request){
+        $umowa = new Umowa();
+        $em = $this->getDoctrine()->getManager();
+
+        $form = $this->getAddForm($umowa, true);
+
+        $form->handleRequest($request);
+
+        if($form->isValid()){
+            $em->persist($umowa);
+            $em->flush();
+            $samorzad = $umowa->getSamorzad();
+            unset($umowa);
+            $umowa = new Umowa();
+            $umowa->setSamorzad($samorzad);
+            unset($form);
+            $form = $this->getAddForm($umowa);
+
+            $this->addFlash('notice', 'Pomyślnie dodano umowę do bazy danych');
+        }
+
+        return $this->render('admin/add.html.twig', array(
+            'form'=>$form->createView(),
+        ));
+    }
+
+    /**
+     * @Route("/admin/umowy", name="admin_umowy_pokaz")
+     */
+    function ShowUmowyAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:Umowa');
+        $qb = $repo->createQueryBuilder($repo);
+        $query = $qb->getQuery();
+
+        $paginator = $this->get('knp_paginator');
+        $umowy = $paginator->paginate(
+          $query,
+          $request->query->getInt('page', 1),
+        );
+
+        return $this->render('admin/umowy.html.twig', array(
+            'umowy'=>$umowy,
+        ));
+    }
+
+    /**
+     * @Route("/admin/umowa/{id}", name="admin_umowa_usun")
+     */
+    function ShowUmowyAction(Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $repo = $em->getRepository('AppBundle:Umowa');
+        $qb = $repo->createQueryBuilder($repo);
+        $query = $qb->getQuery();
+
+        $paginator = $this->get('knp_paginator');
+        $umowy = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+        );
+
+        return $this->render('admin/umowy.html.twig', array(
+            'umowy'=>$umowy,
+        ));
+    }
+}
